a1955f6c25d08a188c5427421f354b562b7feca4
kathryn1995 <kathryn.murie@ucsf.edu>
2024-12-03 15:52:56 -0800

    Update to parse_args_microhaplotype_table_to_json_file in microhaplotype script and fix string in pmotools runner

--- scripts/convertors_to_pmo/microhaplotype_table_to_json_file.py
+++ scripts/convertors_to_pmo/microhaplotype_table_to_json_file.py
@@ -54,7 +54,7 @@ def microhaplotype_table_to_json_file():
                 addCols[addCol] = addCol
 
     # check if input file exists and if output file exists check if --overwrite flag is set
-    Utils.inputOutputFileCheck(args)
+    Utils.inputOutputFileCheckFromArgParse(args)
 
     contents = pd.read_csv(args.file, sep=args.delim)
     output_data = microhaplotype_table_to_pmo_dict(
