1455d9e029b454956451ab01efe73bbc2dfdb8f7
Nicholas Hathaway <nickjhathaway@gmail.com>
2025-01-24 19:26:14 -0500

    update column name for insert loc; added option to add_ref_seqs;

--- scripts/extract_info_from_pmo/extract_insert_of_panels.py
+++ scripts/extract_info_from_pmo/extract_insert_of_panels.py
@@ -15,6 +15,7 @@ def parse_args_extract_insert_of_panels():
     parser.add_argument('--file', type=str, required=True, help='PMO file')
     parser.add_argument('--output', type=str, default="STDOUT", required=False, help='output file')
     parser.add_argument('--overwrite', action = 'store_true', help='If output file exists, overwrite it')
+    parser.add_argument('--add_ref_seqs', action = 'store_true', help='add ref seqs to the output as ref_seq')
 
     return parser.parse_args()
 
@@ -33,11 +34,16 @@ def extract_insert_of_panels():
     # write
     output_target = sys.stdout if args.output == "STDOUT" else open(args.output, "w")
     with output_target as f:
-        f.write("\t".join(["#chrom", "start", "end", "name", "length", "strand", "extra_info"]) + "\n")
+        f.write("\t".join(["#chrom", "start", "end", "target_id", "length", "strand", "extra_info"]))
+        if args.add_ref_seqs:
+            f.write("\tref_seq")
+        f.write("\n")
         for panel_id, bed_locs in panel_bed_locs.items():
             for loc in bed_locs:
-                f.write("\t".join([loc.chrom, str(loc.start), str(loc.end), loc.name, str(loc.score), loc.strand, loc.extra_info]) + "\n")
+                f.write("\t".join([loc.chrom, str(loc.start), str(loc.end), loc.name, str(loc.score), loc.strand, loc.extra_info]))
+                if args.add_ref_seqs:
+                    f.write("\t" + str(loc.ref_seq))
+                f.write("\n")
 
 if __name__ == "__main__":
     extract_insert_of_panels()
-
