b7149d849a6ad2e4130d45f842dda5da58dab817
Nicholas Hathaway <nickjhathaway@gmail.com>
2025-03-16 16:57:22 -0700

    adding jsonschema as requirement;

--- setup.py
+++ setup.py
@@ -2,17 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='pmotools',
-    version='0.1',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'pandas==2.2.2',
         'openpyxl==3.1.4',
         'biopython==1.83',
-        'plotly==5.22.0'
-    ],
-    entry_points={
-        'console_scripts': [
-            'microhaplotype_table_to_json=scripts.microhaplotype_table_to_json:main',
-        ],
-    },
+        'plotly==5.22.0',
+        'jsonschema==4.23.0'
+    ]
 )
