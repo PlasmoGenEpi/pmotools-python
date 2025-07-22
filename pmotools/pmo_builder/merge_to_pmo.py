66c835421049f1767cfde36df22b55ae7e2d40ad
kathryn1995 <kathryn.murie@ucsf.edu>
2025-07-22 13:34:09 -0700

    Add warning if missing names

--- pmotools/pmo_builder/merge_to_pmo.py
+++ pmotools/pmo_builder/merge_to_pmo.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from datetime import date
 import json
-import warnings
 
 
 def panel_info_table_to_json(
@@ -23,26 +22,24 @@ def panel_info_table_to_json(
     project_info = [dict(d) for d in project_info]
     panel_info = json.loads(json.dumps(panel_info))
     mhap_info = json.loads(json.dumps(mhap_info))
-    # TODO: add checks if the entry doesn't exist in the lookup table
-    # TODO: decide whether to take json or dict
 
     # SPECIMEN INFO
     # replace name with project ID
-    replace_key_with_id(specimen_info, project_info,
-                        "project_name", "project_id")
+    missing_projects = replace_key_with_id(specimen_info, project_info,
+                                           "project_name", "project_id")
 
     # EXPERIMENT INFO
     # replace with sequencing_info_id, specimen_id, panel_id
-    replace_key_with_id(library_info, sequencing_info,
-                        'sequencing_info_name', "sequencing_info_id")
-    replace_key_with_id(library_info, specimen_info,
-                        'specimen_name', "specimen_id")
-    replace_key_with_id(
+    missing_sequencing = replace_key_with_id(library_info, sequencing_info,
+                                             'sequencing_info_name', "sequencing_info_id")
+    missing_specimen = replace_key_with_id(library_info, specimen_info,
+                                           'specimen_name', "specimen_id")
+    missing_panels = replace_key_with_id(
         library_info, panel_info["panel_info"], 'panel_name', "panel_id")
 
     # REP MHAPS
     # replace target_name with ID
-    replace_key_with_id(
+    missing_targets = replace_key_with_id(
         mhap_info["representative_microhaplotypes"]["targets"],
         panel_info["target_info"],
         "target_name",
@@ -51,16 +48,21 @@ def panel_info_table_to_json(
 
     # DETECTED MHAPS
     # Replace library_sample_name and bioinformatics_run_name
-    replace_key_with_id(
+    missing_bioinfo_runs = replace_key_with_id(
         mhap_info["detected_microhaplotypes"],
         bioinfo_run_info,
         "bioinformatics_run_name",
         "bioinformatics_run_id"
     )
     lib_sample_lookup = make_lookup(library_info, 'library_sample_name')
+    missing_libs = []
     for detected in mhap_info["detected_microhaplotypes"]:
-        replace_key_with_id(detected["library_samples"],
-                            library_info, 'library_sample_name', 'library_sample_id', lookup=lib_sample_lookup)
+        missing_libs += (replace_key_with_id(detected["library_samples"],
+                                             library_info, 'library_sample_name', 'library_sample_id', lookup=lib_sample_lookup))
+
+    # If any names were missing from reference tables error
+    report_missing_IDs(missing_projects, missing_sequencing, missing_specimen,
+                       missing_panels, missing_targets, missing_bioinfo_runs, missing_libs)
 
     # Build PMO
     pmo_header = generate_pmo_header()
@@ -95,8 +97,15 @@ def replace_key_with_id(
     """
     if not lookup:
         lookup = make_lookup(reference_list, name_key)
+    unique_names = set()
     for entry in target_list:
-        entry[id_key] = lookup.get(str(entry.pop(name_key)))
+        name = str(entry.pop(name_key))
+        unique_names.add(name)
+        entry[id_key] = lookup.get(name)
+    missing_items = list(unique_names - lookup.keys())
+    if (missing_items):
+        print('gonna error')
+    return missing_items
 
 
 def generate_pmo_header():
@@ -105,3 +114,23 @@ def generate_pmo_header():
     pmo_header = {'pmo_version': '1.0.0', 'creation_date': today, 'generation_method': {
         'program_name': 'pmotools-python', 'program_version': 'pmo_version'}}
     return pmo_header
+
+
+def report_missing_IDs(missing_projects, missing_sequencing, missing_specimen, missing_panels, missing_targets, missing_bioinfo_runs, missing_libs):
+    if any([missing_projects, missing_sequencing, missing_specimen, missing_panels, missing_targets, missing_bioinfo_runs, missing_libs]):
+        error_message = 'The following fields were found in one table and not another:\n'
+        if missing_projects:
+            error_message += f'Project names in Specimen Info not in Project Info: {missing_projects}\n'
+        if missing_sequencing:
+            error_message += f'Sequencing names in Library Sample Info not in Sequencing Info: {missing_sequencing}\n'
+        if missing_specimen:
+            error_message += f'Specimen names in Library Sample Info not in Specimen Info: {missing_specimen}\n'
+        if missing_panels:
+            error_message += f'Panel names in Library Sample Info not in Panel Info: {missing_panels}\n'
+        if missing_targets:
+            error_message += f'Target names in Representative Microhaplotypes not in Target Info: {missing_targets}\n'
+        if missing_bioinfo_runs:
+            error_message += f'Bioinformatics run names in Detected Microhaplotypes not in Bioinformatic Run Info: {missing_bioinfo_runs}\n'
+        if missing_libs:
+            error_message += f'Library Sample names in Detected Microhaplotypes not in Library Sample Info: {missing_libs}\n'
+        raise ValueError(error_message)
