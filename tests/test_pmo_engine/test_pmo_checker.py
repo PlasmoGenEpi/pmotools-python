fb559b1675f182bf5312b88e5ae0004ea690c47d
kathryn1995 <kathryn.murie@ucsf.edu>
2025-09-10 17:22:09 -0700

    Setup uv and pyproject. Move project to src

--- tests/test_pmo_engine/test_pmo_checker.py
+++ tests/test_pmo_engine/test_pmo_checker.py
@@ -5,15 +5,15 @@ import unittest
 import json
 from jsonschema import ValidationError
 from pmotools.pmo_engine.pmo_checker import PMOChecker
+from pmotools.utils.schema_loader import load_schema
 
 
 class TestPMOChecker(unittest.TestCase):
     def setUp(self):
         self.working_dir = os.path.dirname(os.path.abspath(__file__))
-        self.pmo_jsonschema_fnp = os.path.join(os.path.dirname(os.path.dirname(self.working_dir)), "etc/portable_microhaplotype_object.schema.json")
-        
-        with open(self.pmo_jsonschema_fnp) as f: 
-            self.pmo_jsonschema_data = json.load(f)
+        self.pmo_jsonschema_data = load_schema(
+            "portable_microhaplotype_object_v0.1.0.schema.json")
+
         self.checker = PMOChecker(self.pmo_jsonschema_data)
         self.pmo_required_base_fields = ["library_sample_info",
                                          "specimen_info",
@@ -40,39 +40,41 @@ class TestPMOChecker(unittest.TestCase):
     def test_pmo_checker_check_for_required_base_fields(self):
 
         pmo_test_object = {
-            "library_sample_info":[],
-            "specimen_info":[],
-            "sequencing_info":[],
-            "panel_info":[],
-            "target_info":[],
-            "targeted_genomes":[],
-            "representative_microhaplotypes":{},
-            "bioinformatics_methods_info":[],
-            "bioinformatics_run_info":[],
-            "detected_microhaplotypes":[],
+            "library_sample_info": [],
+            "specimen_info": [],
+            "sequencing_info": [],
+            "panel_info": [],
+            "target_info": [],
+            "targeted_genomes": [],
+            "representative_microhaplotypes": {},
+            "bioinformatics_methods_info": [],
+            "bioinformatics_run_info": [],
+            "detected_microhaplotypes": [],
             "project_info": [],
-            "pmo_header":{}
+            "pmo_header": {}
         }
         self.checker.check_for_required_base_fields(pmo_test_object)
 
     def test_pmo_checker_check_for_required_base_fields_fail(self):
         pmo_test_object = {
-            "library_sample_info":[],
-            "specimen_info":[],
-            "sequencing_info":[],
-            "panel_info":[],
-            "target_info":[],
-            "targeted_genome":[],
-            "microhaplotypes_info":{},
-            "bioinformatics_methods_infos":[],
-            "bioinformatics_run_info":[],
-            "microhaplotypes_detected":[],
-            "pmo_headers":{}
+            "library_sample_info": [],
+            "specimen_info": [],
+            "sequencing_info": [],
+            "panel_info": [],
+            "target_info": [],
+            "targeted_genome": [],
+            "microhaplotypes_info": {},
+            "bioinformatics_methods_infos": [],
+            "bioinformatics_run_info": [],
+            "microhaplotypes_detected": [],
+            "pmo_headers": {}
         }
-        self.assertRaises(Exception, self.checker.check_for_required_base_fields, pmo_test_object)
+        self.assertRaises(
+            Exception, self.checker.check_for_required_base_fields, pmo_test_object)
 
     def test_pmo_checker_get_required_fields_for_pmo_class(self):
-        self.assertEqual(self.checker.get_required_fields_for_pmo_class("SpecimenInfo"), self.specimen_required_fields)
+        self.assertEqual(self.checker.get_required_fields_for_pmo_class(
+            "SpecimenInfo"), self.specimen_required_fields)
 
     def test_pmo_checker_validate_pmo_json(self):
         with open(os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example.json")) as f:
@@ -86,7 +88,9 @@ class TestPMOChecker(unittest.TestCase):
     def test_pmo_checker_validate_pmo_json_fail(self):
         with open(os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_bad_format.json")) as f:
             pmo_data = json.load(f)
-        self.assertRaises(ValidationError, self.checker.validate_pmo_json, pmo_data)
+        self.assertRaises(
+            ValidationError, self.checker.validate_pmo_json, pmo_data)
+
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_pmo_engine/test_pmo_reader.py
+++ tests/test_pmo_engine/test_pmo_reader.py
@@ -5,6 +5,7 @@ import unittest
 
 from pmotools.pmo_engine.pmo_checker import PMOChecker
 from pmotools.pmo_engine.pmo_reader import PMOReader
+from pmotools.utils.schema_loader import load_schema
 
 
 class TestPMOReader(unittest.TestCase):
@@ -12,32 +13,36 @@ class TestPMOReader(unittest.TestCase):
         self.working_dir = os.path.dirname(os.path.abspath(__file__))
 
     def test_read_in_pmo(self):
-        pmo_data = PMOReader.read_in_pmo(os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example.json"))
-        pmo_data_gz = PMOReader.read_in_pmo(os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example.json.gz"))
-        pmo_data_2 = PMOReader.read_in_pmo(os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json"))
-        pmo_data_2_gz = PMOReader.read_in_pmo(os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json.gz"))
+        pmo_data = PMOReader.read_in_pmo(os.path.join(
+            os.path.dirname(self.working_dir), "data/minimum_pmo_example.json"))
+        pmo_data_gz = PMOReader.read_in_pmo(os.path.join(
+            os.path.dirname(self.working_dir), "data/minimum_pmo_example.json.gz"))
+        pmo_data_2 = PMOReader.read_in_pmo(os.path.join(
+            os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json"))
+        pmo_data_2_gz = PMOReader.read_in_pmo(os.path.join(
+            os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json.gz"))
 
     def test_read_in_pmos(self):
         pmo_data_list = PMOReader.read_in_pmos([os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example.json"),
-                                            os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json")])
+                                                os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json")])
 
         pmo_data_list_gz = PMOReader.read_in_pmos([os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example.json.gz"),
-                                            os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json.gz")])
+                                                   os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json.gz")])
 
         pmo_data_list_mix = PMOReader.read_in_pmos([os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example.json.gz"),
-                                            os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json")])
+                                                    os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json")])
 
     def test_combine_multiple_pmos(self):
         pmo_data_list = PMOReader.read_in_pmos([
-            os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example.json"),
-            os.path.join(os.path.dirname(self.working_dir), "data/minimum_pmo_example_2.json")
+            os.path.join(os.path.dirname(self.working_dir),
+                         "data/minimum_pmo_example.json"),
+            os.path.join(os.path.dirname(self.working_dir),
+                         "data/minimum_pmo_example_2.json")
         ])
         combined_pmo = PMOReader.combine_multiple_pmos(pmo_data_list)
         # validate with schema
-        pmo_jsonschema_fnp = os.path.join(os.path.dirname(os.path.dirname(self.working_dir)),
-                                               "etc/portable_microhaplotype_object.schema.json")
-        with open(pmo_jsonschema_fnp) as f:
-            pmo_jsonschema_data = json.load(f)
+        pmo_jsonschema_data = load_schema(
+            "portable_microhaplotype_object_v0.1.0.schema.json")
         checker = PMOChecker(pmo_jsonschema_data)
         checker.validate_pmo_json(combined_pmo)
         # check against expected
@@ -48,7 +53,6 @@ class TestPMOReader(unittest.TestCase):
         combined_pmo.pop("pmo_header")
 
         self.assertEqual(expected_pmo, combined_pmo)
-
 
 
 if __name__ == "__main__":
